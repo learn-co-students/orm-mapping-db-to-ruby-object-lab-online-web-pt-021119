50c0fa674da1137483ba107684dafb71cd307794
Cody Yantis <cyantis@gmail.com>
2019-04-27 22:18:25 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -26,9 +26,62 @@ class Student
       FROM students
       WHERE name = ?
     SQL
-    
+
     student = DB[:conn].execute(sql, name).first
     self.new_from_db(student)
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql).collect {|row| self.new_from_db(row)}
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade != 12
+    SQL
+
+    DB[:conn].execute(sql).collect {|row| self.new_from_db(row)}
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql,num).collect {|row| self.new_from_db(row)}
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+
+    student = DB[:conn].execute(sql).first
+    self.new_from_db(student)
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql,grade).collect {|row| self.new_from_db(row)}
   end
 
   def save
